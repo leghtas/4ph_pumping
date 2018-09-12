corrected mode ordering and non-linearities computation

circuit.py
@@ -123,6 +123,20 @@ def get_factor(which):
         factor = factor*factorial(which.count(elt))
     return factor
 
+def permute(to_add, old=[]):
+    new = []
+    for _to_add in to_add:
+        new_list = old+[_to_add]
+        new_to_add = to_add.copy()
+        new_to_add.remove(_to_add)
+        if len(new_to_add)>=1:
+            permuted = permute(new_to_add, new_list)
+            for perm in permuted:
+                new.append(perm)
+        else:
+            new.append(new_list)
+    return new
+
 class Circuit(object):
 
     def __init__(self):
@@ -138,6 +152,7 @@ class Circuit(object):
         self.store_anyL('T', self.max_order)
         self.to_compare_0 = None
         self.to_compare_2 = None
+        self.permutations = None
         
     def remove_params(self, symbol_list):
         variables = []
@@ -286,7 +301,7 @@ class Circuit(object):
             quad = self.get_quadratic_form(T)
         return quad
 
-    def get_freqs_kerrs(self, particular=None, **kwargs):
+    def get_freqs_kerrs(self, particular=None, return_components=False, **kwargs):
 
         res = self.get_normal_mode_frame(**kwargs)
         res1, res2, P, w2 = res
@@ -310,7 +325,7 @@ class Circuit(object):
         popt3 = np.array([Hess3_r[0, 0, 0]/6, Hess3_r[1, 1, 1]/6]) # coeff devant le phi**3
         popt4 = np.array([Hess4_r[0, 0, 0, 0]/24, Hess4_r[1, 1, 1, 1]/24]) # coeff devant le phi**4
         
-        ZPF = popt2**(-1./4)
+        ZPF = popt2**(-1./4)/4**0.5 # hbar*w/2 is the term in front of a^+.a in the hamiltonian coming from phi**2, the other half is from dphi**2 
         
         if particular is not None:
             factor, power = get_factor(particular)
@@ -329,13 +344,16 @@ class Circuit(object):
         else:
             Xip = None
             
-        Xi2 = popt2*(ZPF**2)/2/np.pi # freq en Hz
-        Xi3 = 2 * popt3*(ZPF**3)/2/np.pi #coeff devant a^2.a^+
+        # factor 4 see former remark so in front of phi**2 we got w/4 (one 2 come from developping (a^+ + a)**2, the other from the kinetic part)
+        Xi2 = 4 * popt2*(ZPF**2)/2/np.pi # freq en Hz : coeff devant a^+.a (*2 to get whole freq)
+        Xi3 = 3 * popt3*(ZPF**3)/2/np.pi #coeff devant a^2.a^+
         Xi4 = 6 * popt4*(ZPF**4)/2/np.pi #coeff devant a^2.a^+2
 
 #        check_Xi2 = w2**0.5/2/np.pi
-
-        return res1, res2, Xi2, Xi3, Xi4, Xip
+        if return_components:
+            return res1, res2, Xi2, Xi3, Xi4, Xip
+        else:
+            return res1, res2, Xi2, Xi3, Xi4, Xip, P
 
     def get_freqs_only(self,  **kwargs):
         res = self.get_normal_mode_frame(**kwargs)
@@ -347,17 +365,21 @@ class Circuit(object):
         res1, U0 = self.get_U_matrix(mode = 'analytical', **kwargs)
         res2, T0 = self.get_T_matrix(mode = 'analytical', **kwargs)
         
-        w0, v0 = self.reorder(*(nl.eigh(T0)), 0, debug=False)
+        w0, v0 = nl.eigh(T0)
+#        w0, v0 = self.reorder(*(nl.eigh(T0)), 0, debug=False)
         sqrtw = sl.sqrtm(np.diag(w0))
 
         U1 = np.dot(np.dot(nl.inv(v0), U0), v0)
         U2 = np.dot(np.dot(nl.inv(sqrtw), U1), nl.inv(sqrtw))
         w2, v2 = nl.eigh(U2)
         
-        w2, v2 = self.reorder(w2, v2, 2, debug=False)
+#        w2, v2 = self.reorder(w2, v2, 2, debug=False)
 
 
         P = np.dot(np.dot(v0, nl.inv(sqrtw)), v2)
+#        print(w0)
+        
+        w2, P = self.reorder(w2, P, 2)
 
         invP = np.dot(np.dot(nl.inv(v2), nl.inv(sqrtw)), nl.inv(v0))
 
@@ -365,33 +387,60 @@ class Circuit(object):
         
         wU3 = np.diag(np.dot(np.dot(invP, U0), P))
 
-        return res1, res2, P, wU3
+        return res1, res2, P, w2
     
     def reorder(self, values, vectors, either0_or2, debug=False):
         dim = len(values)
         vectors = vectors.T
         order = []
+        if either0_or2 ==2:
+#            print(vectors)
+            val, vec = nl.eigh(vectors.T)
+#            print('val propres 0 =' +str(val))
         
 #        print(self.to_compare_0, self.to_compare_2)
         
         to_compares = [self.to_compare_0, self.to_compare_2]
         jj = int(either0_or2/2)
         if to_compares[jj] is None:
-            to_compare = np.array(vectors)
+            to_compare = np.eye(dim)
+            self.permutations = permute([ii for ii in range(dim)])
         else:
-            to_compare = to_compares[jj]
+            to_compare = np.eye(dim)
+#            to_compare = to_compares[jj]
             
-
+        distances = []
+        sgns = []
         for ii, vector in enumerate(vectors):
             if debug:
                 print('comparison')
                 print(np.dot(to_compare, vector))
-                
-            index_max = np.argmax(np.abs(np.dot(to_compare, vector)))
-            sgn = round(vector[index_max]/np.abs(vector[index_max]))
-#            print(sgn)
-            vectors[ii] = vector*sgn
+            dists, sgn = comp_dist(vector, to_compare)
+            distances.append(dists)
+            sgns.append(sgn)
+#            if either0_or2==2:
+#                print(dists)
+            index_max = np.argmin(dists)
+#            vectors[ii] = vector*sgn
             order.append(index_max)
+        
+        best_sum_dist = np.inf
+        best_perm = None
+        for perm in self.permutations:
+            sum_dist = 0
+            for ii in perm:
+                sum_dist += distances[ii][perm[ii]]
+            if sum_dist<best_sum_dist:
+                best_perm = perm
+                best_sum_dist = sum_dist
+                
+        order=best_perm
+#        order=[0,1,2,3,4]
+        for ii in range(dim):
+            vectors[ii]=vectors[ii]*sgns[ii][order[ii]]
+#        if either0_or2==2:
+#            print(order)
+        
 #        print(order)
         
 #        order_bis = [[] for i in range(dim)]
@@ -454,3 +503,13 @@ class Circuit(object):
         values_ordered = np.array(values_ordered)
     #    print(order)
         return values_ordered, vectors_ordered
+    
+def comp_dist(a, b):
+    ret=[]
+    sgn=[]
+    for ii in b:
+        dists = np.array([(((a-ii)**2).sum())**0.5, (((a+ii)**2).sum())**0.5])
+        argmin = np.argmin(dists)
+        ret.append(dists[argmin])
+        sgn.append(argmin*(-2)+1)
+    return np.array(ret), np.array(sgn)

circuit_pumping2_snail_ind.py
@@ -29,7 +29,7 @@ e=c.e
 
 class CircuitPump2Snail(c.Circuit):
 
-    def __init__(self, wa, Za, wb, Zb, Cca, Ccb, Cc, EJ, alpha, n, ECj = None,
+    def __init__(self, wa, Za, wb, Zb, Cca, Ccb, Cc, EJ, dLJ, alpha, n, ECj = None,
                  printParams=True):
         
         # from http://arxiv.org/abs/1602.01793
@@ -61,6 +61,8 @@ class CircuitPump2Snail(c.Circuit):
         self.ECj = ECj
         
         self.EJ = EJ
+        dLJ = dLJ/2
+        self.dELJ = phi0**2/dLJ
         self.n = n
         self.alpha = alpha
         
@@ -70,15 +72,17 @@ class CircuitPump2Snail(c.Circuit):
         
         self.U_str = 'ELa/2/hbar*pa**2 \
                       + ELb/2/hbar*pb**2 \
-                      -alpha*(EJ/hbar)*cos(pc) \
-                      -n*(EJ/hbar)*cos((phi_ext_0-pc)/n) \
+                      + 2* dELJ/2/hbar*pcl**2 \
+                      - alpha*(EJ/hbar)*cos(pc) \
+                      - n*(EJ/hbar)*cos((phi_ext_0-pc)/n) \
                       + 0.0*ELb/2/hbar*pca**2'
                  
         self.T_str = '(1/16.)*(hbar/ECa)*(dpa)**2 \
                       + (1/16.)*(hbar/ECb)*(dpb)**2\
-                      + (1/16.)*(alpha+1/n)*hbar*(1/ECj+1/ECc)*(dpc)**2 \
+                      + (1/16.)*(hbar/ECc)*(2*dpcl+dpc)**2\
+                      + (1/16.)*(alpha+1/n)*hbar*1/ECj*(dpc)**2 \
                       + (1/16.)*(hbar/ECca)*(dpca)**2 \
-                      + (1/16.)*(hbar/ECcb)*(dpc+dpca+dpa-dpb)**2'
+                      + (1/16.)*(hbar/ECcb)*(dpc+dpca+dpa+2*dpcl-dpb)**2'
                       # + (1/16.)*(hbar/ECca)*(dpc+dpca+dpa-dpb)**2'
 
                 
@@ -146,12 +150,11 @@ class CircuitPump2Snail(c.Circuit):
         Hess3_r = Hess3U(res1, P=P)
         Hess4_r = Hess4U(res1, P=P)
         
-        popt2 = np.array([Hess2_r[0, 0]/2, Hess2_r[1, 1]/2, Hess2_r[2, 2]/2, Hess2_r[3, 3]/2])
-        popt3 = np.array([Hess3_r[0, 0, 0]/6, Hess3_r[1, 1, 1]/6, Hess3_r[2, 2, 2]/6, Hess3_r[3, 3, 3]/6])
-        popt4 = np.array([Hess4_r[0, 0, 0, 0]/24, Hess4_r[1, 1, 1, 1]/24, Hess4_r[2, 2, 2, 2]/24, Hess4_r[3, 3, 3, 3]/4])
+        popt2 = np.array([Hess2_r[0, 0]/2, Hess2_r[1, 1]/2, Hess2_r[2, 2]/2, Hess2_r[3, 3]/2, Hess2_r[4, 4]/2])
+        popt3 = np.array([Hess3_r[0, 0, 0]/6, Hess3_r[1, 1, 1]/6, Hess3_r[2, 2, 2]/6, Hess3_r[3, 3, 3]/6, Hess3_r[4, 4, 4]/2])
+        popt4 = np.array([Hess4_r[0, 0, 0, 0]/24, Hess4_r[1, 1, 1, 1]/24, Hess4_r[2, 2, 2, 2]/24, Hess4_r[3, 3, 3, 3]/4, Hess4_r[4, 4, 4, 4]/4])
         
-        ZPF = popt2**(-1./4)
-        
+        ZPF = popt2**(-1./4)/4**0.5
 
         if particulars is not None:
             Xip = []
@@ -172,7 +175,7 @@ class CircuitPump2Snail(c.Circuit):
         else:
             Xip = None
             
-        Xi2 = popt2*(ZPF**2)/2/np.pi # freq en Hz
+        Xi2 = 4 * popt2*(ZPF**2)/2/np.pi # freq en Hz
         Xi3 = 2 * popt3*(ZPF**3)/2/np.pi #coeff devant a^2.a^+
         Xi4 = 6 * popt4*(ZPF**4)/2/np.pi #coeff devant a^2.a^+2
 #        check_Xi2 = w2**0.5/2/np.pi

circuit_pumping2_snail_simple.py
@@ -29,11 +29,13 @@ e=c.e
 
 class CircuitPump2Snail(c.Circuit):
 
-    def __init__(self, wa, Za, wb, Zb, Cca, Ccb, Cc, EJ, alpha, n, ECj = None,
+    def __init__(self, wa, Za, Cc, w, EJ, alpha, n, ECj = None,
                  printParams=True):
         
         # from http://arxiv.org/abs/1602.01793
         _, _, LJ = c.get_w_Z_LJ_from_E(1, 1, EJ)
+        Leq = 1/(alpha+1/n)*LJ 
+        _, _, Eeq = c.get_E_from_w(1, 1, Leq) 
         
         phia_zpf = (1/phi0) * (np.sqrt((hbar/2)*Za))  # phiZPF
         na_zpf = (1/(2*e)) * (np.sqrt(hbar/2/Za))  # nZPF
@@ -41,44 +43,35 @@ class CircuitPump2Snail(c.Circuit):
         La = Za/wa
         self.ELa = phi0**2/La
         self.ECa = e**2/2/Ca
-        self.ECca = e**2/2/Cca
-        
-        
-        phib_zpf = (1/phi0) * (np.sqrt((hbar/2)*Zb))  # phiZPF
-        nb_zpf = (1/(2*e)) * (np.sqrt(hbar/2/Zb))  # nZPF
-        Cb = 1/(Zb*wb)
-        Lb = Zb/wb
-        self.ELb = phi0**2/Lb
-        self.ECb = e**2/2/Cb
-        self.ECcb = e**2/2/Ccb
         
         self.ECc = e**2/2/Cc
 
-        omega_plasma = 2*pi*24e9
-        CJ = 1/(omega_plasma**2*LJ) # each junction has 2*LJ
-        if ECj == None:
-            ECj = e**2/2/CJ
-        self.ECj = ECj
+#        omega_plasma = 2*pi*24e9
+#        CJ = 1/(omega_plasma**2*LJ) # each junction has 2*LJ
+#        if ECj == None:
+#            ECj = e**2/2/CJ
+#        self.ECj = ECj
         
         self.EJ = EJ
+        self.EJ = EJ
+        self.EC = (hbar*w)**2/8/Eeq
         self.n = n
         self.alpha = alpha
-        
         self.varying_params={'phi_ext_0':0}
 #        self.phi_ext_0 = 0
 #        self.varying_params={'ECca':0}
         
         self.U_str = 'ELa/2/hbar*pa**2 \
-                      + ELb/2/hbar*pb**2 \
-                      -alpha*(EJ/hbar)*cos(pc) \
-                      -n*(EJ/hbar)*cos((phi_ext_0-pc)/n) \
-                      + 0.0*ELb/2/hbar*pca**2'
+                      - alpha*(EJ/hbar)*cos(ps) \
+                      - n*(EJ/hbar)*cos((phi_ext_0-ps)/n)'
+#                      - EJ/hbar*cos(ps)+0*phi_ext_0'
                  
         self.T_str = '(1/16.)*(hbar/ECa)*(dpa)**2 \
-                      + (1/16.)*(hbar/ECb)*(dpb)**2\
-                      + (1/16.)*(alpha+1/n)*hbar*(1/ECj+1/ECc)*(dpc)**2 \
-                      + (1/16.)*(hbar/ECca)*(dpca)**2 \
-                      + (1/16.)*(hbar/ECcb)*(dpc+dpca+dpa-dpb)**2'
+                      + (1/16.)*(hbar/EC)*(dps)**2\
+                      + (1/16.)*(hbar/ECc)*(dps-dpa)**2'
+#                      + (1/16.)*(alpha+1/n)*hbar*(1/ECj+1/ECc)*(dpc)**2 \
+#                      + (1/16.)*(hbar/ECca)*(dpca)**2 \
+#                      + (1/16.)*(hbar/ECcb)*(dpc+dpca+dpa-dpb)**2'
                       # + (1/16.)*(hbar/ECca)*(dpc+dpca+dpa-dpb)**2'
 
                 
@@ -88,39 +81,29 @@ class CircuitPump2Snail(c.Circuit):
             print("Za = "+str(Za)+"Ohm")
             print("La = "+str(La*1e9)+" nH")
             print("Ca = "+str(Ca*1e15)+" fF")
-            print("Cca = "+str(Cca*1e15)+" fF")
             print("phia_zpf = "+str(phia_zpf))
             print("na_zpf = "+str(na_zpf))
-            
-            print("fb = "+str(wb/2/pi*1e-9)+"GHz")
-            print("Zb = "+str(Zb)+"Ohm")
-            print("Lb = "+str(Lb*1e9)+" nH")
-            print("Cb = "+str(Cb*1e15)+" fF")
-            print("Ccb = "+str(Cca*1e15)+" fF")
-            print("phib_zpf = "+str(phib_zpf))
-            print("nb_zpf = "+str(nb_zpf))
             
             print("Cc = "+str(Cc*1e15)+" fF")
             
             print("ELa/h = "+str(1e-9*self.ELa/hbar/2/pi)+" GHz")
             print("ECa/h = "+str(1e-9*self.ECa/hbar/2/pi)+" GHz")
-            print("ELb/h = "+str(1e-9*self.ELb/hbar/2/pi)+" GHz")
-            print("ECb/h = "+str(1e-9*self.ECb/hbar/2/pi)+" GHz")
             
+            print('Leq = %.3f nH'%(Leq/1e-9))
             print("LJ_grosse = "+str(LJ*1e9)+" nH")
             print("LJ_petite = "+str(LJ/alpha*1e9)+" nH")
-            print("CJ = "+str(CJ*1e15)+" fF")
-            print("fJ = "+str(1/(LJ*CJ)**0.5*1e-9/2/pi)+" GHz")
+#            print("CJ = "+str(CJ*1e15)+" fF")
+#            print("fJ = "+str(1/(LJ*CJ)**0.5*1e-9/2/pi)+" GHz")
             
-            LJ_snail = 1/(alpha+1/n)*LJ
-            CJ_snail = (alpha+1/n)*CJ
-            print("LJ_snail = "+str(LJ_snail*1e9)+" nH")
-            print("CJ_snail = "+str(CJ_snail*1e15)+" fF")
-            print("f_snail = "+str(1/(LJ_snail*CJ_snail)**0.5*1e-9/2/pi)+" GHz")
+#            LJ_snail = 1/(alpha+1/n)*LJ
+#            CJ_snail = (alpha+1/n)*CJ
+#            print("LJ_snail = "+str(LJ_snail*1e9)+" nH")
+#            print("CJ_snail = "+str(CJ_snail*1e15)+" fF")
+#            print("f_snail = "+str(1/(LJ_snail*CJ_snail)**0.5*1e-9/2/pi)+" GHz")
             
             print("EJ/h = "+str(1e-9*self.EJ/hbar/2/pi)+" GHz")
 
-            print("CJ per junction = "+str(CJ*1e15)+str(" fF"))
+#            print("CJ per junction = "+str(CJ*1e15)+str(" fF"))
             
             
 
@@ -146,12 +129,11 @@ class CircuitPump2Snail(c.Circuit):
         Hess3_r = Hess3U(res1, P=P)
         Hess4_r = Hess4U(res1, P=P)
         
-        popt2 = np.array([Hess2_r[0, 0]/2, Hess2_r[1, 1]/2, Hess2_r[2, 2]/2, Hess2_r[3, 3]/2])
-        popt3 = np.array([Hess3_r[0, 0, 0]/6, Hess3_r[1, 1, 1]/6, Hess3_r[2, 2, 2]/6, Hess3_r[3, 3, 3]/6])
-        popt4 = np.array([Hess4_r[0, 0, 0, 0]/24, Hess4_r[1, 1, 1, 1]/24, Hess4_r[2, 2, 2, 2]/24, Hess4_r[3, 3, 3, 3]/4])
+        popt2 = np.array([Hess2_r[0, 0]/2, Hess2_r[1, 1]/2])
+        popt3 = np.array([Hess3_r[0, 0, 0]/6, Hess3_r[1, 1, 1]/6])
+        popt4 = np.array([Hess4_r[0, 0, 0, 0]/24, Hess4_r[1, 1, 1, 1]/24])
         
-        ZPF = popt2**(-1./4)
-        
+        ZPF = popt2**(-1./4)/4**0.5 # hbar*w/2 is the term in front of a^+.a in the hamiltonian coming from phi**2, the other half is from dphi**2 
 
         if particulars is not None:
             Xip = []
@@ -172,8 +154,9 @@ class CircuitPump2Snail(c.Circuit):
         else:
             Xip = None
             
-        Xi2 = popt2*(ZPF**2)/2/np.pi # freq en Hz
-        Xi3 = 2 * popt3*(ZPF**3)/2/np.pi #coeff devant a^2.a^+
+        # factor 4 see former remark so in front of phi**2 we got w/4 (one 2 come from developping (a^+ + a)**2, the other from the kinetic part)
+        Xi2 = 4 * popt2*(ZPF**2)/2/np.pi # freq en Hz : coeff devant a^+.a (*2 to get whole freq)
+        Xi3 = 3 * popt3*(ZPF**3)/2/np.pi #coeff devant a^2.a^+
         Xi4 = 6 * popt4*(ZPF**4)/2/np.pi #coeff devant a^2.a^+2
 #        check_Xi2 = w2**0.5/2/np.pi
         if return_components:

pumping2_snail_ind.py
@@ -9,7 +9,7 @@ import scipy.constants as sc
 import numpy as np
 import matplotlib.pyplot as plt
 import circuit
-import circuit_pumping2_snail as cp2s
+import circuit_pumping2_snail_ind as cp2s
 import scipy.linalg as sl
 import numpy.linalg as nl
 from scipy.misc import factorial
@@ -39,22 +39,22 @@ plt.close('all')
 wa, Za = [5.8e9*2*np.pi, 120]
 wb, Zb = [9.6e9*2*np.pi, 120]#[9.6e9*2*np.pi, 120]
 
-Cca, Ccb, Cc = [(252)*1e-15,(252)*1e-15, 25e-15]
+Cca, Ccb, Cc = [(150)*1e-15,(150)*1e-15, 25e-15]
 
-LJ, dLJ, alpha, n = [2000e-12, 500e-12, 0.33, 3] #dLJ, linear part of inductance for sna
+LJ, dLJ, alpha, n = [5000e-12, 500e-12, 0.33, 3] #dLJ, linear part of inductance for sna
 
 
 
-_, _, EJ = circuit.get_E_from_w(1, 1, LJ+dLJ) #dLJ should be 0 to compute non-linearities !!!!!
+_, _, EJ = circuit.get_E_from_w(1, 1, LJ) #dLJ should be 0 to compute non-linearities !!!!!
 
 #EJ, LJ, I0 = circuit.convert_EJ_LJ_I0(I0=I0)
 
 
-c = cp2s.CircuitPump2Snail(wa, Za, wb, Zb, Cca, Ccb, Cc, EJ, alpha, n) 
+c = cp2s.CircuitPump2Snail(wa, Za, wb, Zb, Cca, Ccb, Cc, EJ, dLJ, alpha, n) 
 
 min_phi = 0*2*pi
 max_phi = 1*2*pi
-Npts = 101
+Npts = 11
 phiVec = np.linspace(min_phi, max_phi, Npts)
 ng_sweep = np.linspace(-1, 1, 21)
 
@@ -96,16 +96,16 @@ if 1==0:
 # Get freqs and Kerrs v.s. flux
 if 1==1:
     
-    Xi2 = np.zeros((4, len(phiVec)))
-    Xi3 = np.zeros((4, len(phiVec)))
-    Xi4 = np.zeros((4, len(phiVec)))
-    check_Xi2 = np.zeros((4, len(phiVec)))
+    Xi2 = np.zeros((5, len(phiVec)))
+    Xi3 = np.zeros((5, len(phiVec)))
+    Xi4 = np.zeros((5, len(phiVec)))
+    check_Xi2 = np.zeros((5, len(phiVec)))
     Xi_pa2pb = np.zeros(len(phiVec))
     Xi_ac = np.zeros(len(phiVec))
     Xi_bc = np.zeros(len(phiVec))
     resx = np.zeros(len(phiVec))
     resy = np.zeros(len(phiVec))
-    comp = np.zeros((4, 4, len(phiVec)))
+    comp = np.zeros((5, 5, len(phiVec)))
     for kk, xx in enumerate(phiVec):
         _res  = c.get_freqs_kerrs(particulars=[(1,1,2), (1,1,3,3), (2,2,3,3)], return_components=True, phi_ext_0=xx)
         res1, res2, Xi2s, Xi3s, Xi4s, Xi_p, P= _res
@@ -127,38 +127,51 @@ if 1==1:
     ax0.plot(phiVec/2/pi, Xi2[1,:]/1e9, '.', label= 'f1')
     ax0.plot(phiVec/2/pi, Xi2[2,:]/1e9, '.', label= 'f2')
     ax0.plot(phiVec/2/pi, Xi2[3,:]/1e9, '.', label= 'f3')
+    ax0.plot(phiVec/2/pi, Xi2[4,:]/1e9, '.', label= 'f4')
     ax0.legend()
     ax0.set_ylabel('GHz')
     
-    fig00, ax00 = plt.subplots(2,2,figsize=(12,6))
+    fig00, ax00 = plt.subplots(3,2,figsize=(12,6))
     ax00[0,0].plot(phiVec/2/pi, comp[0, 0,:], '.', label= 'f0/pa')    
     ax00[0,0].plot(phiVec/2/pi, comp[0, 1,:], '.', label= 'f0/pb')    
     ax00[0,0].plot(phiVec/2/pi, comp[0, 2,:], '.', label= 'f0/pc')    
     ax00[0,0].plot(phiVec/2/pi, comp[0, 3,:], '.', label= 'f0/pca')   
-    ax00[0,0].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[0], '.', label= 'norm') 
+    ax00[0,0].plot(phiVec/2/pi, comp[0, 4,:], '.', label= 'f0/pcl')   
+#    ax00[0,0].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[0], '.', label= 'norm') 
 
     ax00[1,0].plot(phiVec/2/pi, comp[1, 0,:], '.', label= 'f1/pa')    
     ax00[1,0].plot(phiVec/2/pi, comp[1, 1,:], '.', label= 'f1/pb')    
     ax00[1,0].plot(phiVec/2/pi, comp[1, 2,:], '.', label= 'f1/pc')    
     ax00[1,0].plot(phiVec/2/pi, comp[1, 3,:], '.', label= 'f1/pca')   
-    ax00[1,0].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[1], '.', label= 'norm') 
+    ax00[1,0].plot(phiVec/2/pi, comp[1, 4,:], '.', label= 'f1/pcl')   
+#    ax00[1,0].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[1], '.', label= 'norm') 
 
     ax00[0,1].plot(phiVec/2/pi, comp[2, 0,:], '.', label= 'f2/pa')    
     ax00[0,1].plot(phiVec/2/pi, comp[2, 1,:], '.', label= 'f2/pb')    
     ax00[0,1].plot(phiVec/2/pi, comp[2, 2,:], '.', label= 'f2/pc')    
     ax00[0,1].plot(phiVec/2/pi, comp[2, 3,:], '.', label= 'f2/pca')   
-    ax00[0,1].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[2], '.', label= 'norm') 
+    ax00[0,1].plot(phiVec/2/pi, comp[2, 4,:], '.', label= 'f2/pcl')   
+#    ax00[0,1].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[2], '.', label= 'norm') 
 
     ax00[1,1].plot(phiVec/2/pi, comp[3, 0,:], '.', label= 'f3/pa')    
     ax00[1,1].plot(phiVec/2/pi, comp[3, 1,:], '.', label= 'f3/pb')    
     ax00[1,1].plot(phiVec/2/pi, comp[3, 2,:], '.', label= 'f3/pc')    
     ax00[1,1].plot(phiVec/2/pi, comp[3, 3,:], '.', label= 'f3/pca')   
-    ax00[1,1].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[3], '.', label= 'norm') 
+    ax00[1,1].plot(phiVec/2/pi, comp[3, 4,:], '.', label= 'f3/pcl')   
+#    ax00[1,1].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[3], '.', label= 'norm') 
+    
+    ax00[2,1].plot(phiVec/2/pi, comp[4, 0,:], '.', label= 'f4/pa')    
+    ax00[2,1].plot(phiVec/2/pi, comp[4, 1,:], '.', label= 'f4/pb')    
+    ax00[2,1].plot(phiVec/2/pi, comp[4, 2,:], '.', label= 'f4/pc')    
+    ax00[2,1].plot(phiVec/2/pi, comp[4, 3,:], '.', label= 'f4/pca')   
+    ax00[2,1].plot(phiVec/2/pi, comp[4, 4,:], '.', label= 'f4/pcl')   
+#    ax00[2,1].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[4], '.', label= 'norm') 
 
     ax00[0,0].legend()
     ax00[0,1].legend()
     ax00[1,0].legend()    
     ax00[1,1].legend()
+    ax00[2,1].legend()
     
     print('\nf0 = %.3f GHz\n'%(Xi2[0,:][0]/1e9)+'f1 = %.3f GHz\n'%(Xi2[1,:][0]/1e9)+'f2 = %.3f GHz\n'%(Xi2[2,:][0]/1e9)+'f3 = %.3f GHz\n'%(Xi2[3,:][0]/1e9))
     
@@ -180,6 +193,8 @@ if 1==1:
     ax[3,1].plot(phiVec/2/pi, Xi3[3,:]/1e6)
     ax[0,1].set_title('c3')
     
+    print('K0 = %.3f MHz\n'%(2*Xi4[0,:][0]/1e6)+'K1 = %.3f MHz\n'%(2*Xi4[1,:][0]/1e6)+'K2 = %.3f MHz\n'%(2*Xi4[2,:][0]/1e6)+'K3 = %.3f MHz\n'%(2*Xi4[3,:][0]/1e6))
+
     ax[0,2].plot(phiVec/2/pi, Xi4[0,:]/1e6)    
     ax[1,2].plot(phiVec/2/pi, Xi4[1,:]/1e6)
     ax[2,2].plot(phiVec/2/pi, Xi4[2,:]/1e6)

pumping2_snail_simple.py
@@ -9,7 +9,7 @@ import scipy.constants as sc
 import numpy as np
 import matplotlib.pyplot as plt
 import circuit
-import circuit_pumping2_snail as cp2s
+import circuit_pumping2_snail_simple as cp2s
 import scipy.linalg as sl
 import numpy.linalg as nl
 from scipy.misc import factorial
@@ -36,25 +36,24 @@ pi = np.pi
 plt.close('all')
 
 
-wa, Za = [5.8e9*2*np.pi, 120]
-wb, Zb = [9.6e9*2*np.pi, 120]#[9.6e9*2*np.pi, 120]
+wa, Za = [5e9*2*np.pi, 120]
 
-Cca, Ccb, Cc = [(252)*1e-15,(252)*1e-15, 25e-15]
+Cc = 2.8*1e-15
 
-LJ, dLJ, alpha, n = [2000e-12, 500e-12, 0.33, 3] #dLJ, linear part of inductance for sna
+LJ, alpha, n = [12000e-12/3, 1/1, 3] #dLJ, linear part of inductance for sna
+w = 10.0e9*2*pi
 
 
-
-_, _, EJ = circuit.get_E_from_w(1, 1, LJ+dLJ) #dLJ should be 0 to compute non-linearities !!!!!
+_, _, EJ = circuit.get_E_from_w(1, 1, LJ) #dLJ should be 0 to compute non-linearities !!!!!
 
 #EJ, LJ, I0 = circuit.convert_EJ_LJ_I0(I0=I0)
 
 
-c = cp2s.CircuitPump2Snail(wa, Za, wb, Zb, Cca, Ccb, Cc, EJ, alpha, n) 
+c = cp2s.CircuitPump2Snail(wa, Za, Cc, w, EJ, alpha, n) 
 
 min_phi = 0*2*pi
 max_phi = 1*2*pi
-Npts = 101
+Npts = 1001
 phiVec = np.linspace(min_phi, max_phi, Npts)
 ng_sweep = np.linspace(-1, 1, 21)
 
@@ -95,26 +94,25 @@ if 1==0:
 
 # Get freqs and Kerrs v.s. flux
 if 1==1:
-    
-    Xi2 = np.zeros((4, len(phiVec)))
-    Xi3 = np.zeros((4, len(phiVec)))
-    Xi4 = np.zeros((4, len(phiVec)))
-    check_Xi2 = np.zeros((4, len(phiVec)))
-    Xi_pa2pb = np.zeros(len(phiVec))
-    Xi_ac = np.zeros(len(phiVec))
-    Xi_bc = np.zeros(len(phiVec))
+    n_modes = 2
+    Xi2 = np.zeros((n_modes, len(phiVec)))
+    Xi3 = np.zeros((n_modes, len(phiVec)))
+    Xi4 = np.zeros((n_modes, len(phiVec)))
+    check_Xi2 = np.zeros((n_modes, len(phiVec)))
+    Xi_a2s = np.zeros(len(phiVec))
+    Xi_a2s2 = np.zeros(len(phiVec))
     resx = np.zeros(len(phiVec))
     resy = np.zeros(len(phiVec))
-    comp = np.zeros((4, 4, len(phiVec)))
+    comp = np.zeros((n_modes, n_modes, len(phiVec)))
     for kk, xx in enumerate(phiVec):
-        _res  = c.get_freqs_kerrs(particulars=[(1,1,2), (1,1,3,3), (2,2,3,3)], return_components=True, phi_ext_0=xx)
+        _res  = c.get_freqs_kerrs(particulars=[(0,0,1), (0,0,1,1)], return_components=True, phi_ext_0=xx)
         res1, res2, Xi2s, Xi3s, Xi4s, Xi_p, P= _res
         Xi2[:, kk] = Xi2s
         Xi3[:, kk] = Xi3s
-        Xi4[:, kk] = Xi4s
-        Xi_pa2pb[kk] = Xi_p[0]
-        Xi_ac[kk] = 4*Xi_p[1]
-        Xi_bc[kk] = 4*Xi_p[2]
+        Xi4[:, kk] = 2*Xi4s
+        Xi_a2s[kk] = Xi_p[0]
+#        print(Xi_p[1])
+        Xi_a2s2[kk] = 4*Xi_p[1]
         resx[kk] = res1[0]
         resy[kk] = res1[1]
         comp[:, :, kk] = (P.T) 
@@ -125,88 +123,88 @@ if 1==1:
     fig0, ax0 = plt.subplots(figsize=(12,6))
     ax0.plot(phiVec/2/pi, Xi2[0,:]/1e9, '.', label= 'f0')    
     ax0.plot(phiVec/2/pi, Xi2[1,:]/1e9, '.', label= 'f1')
-    ax0.plot(phiVec/2/pi, Xi2[2,:]/1e9, '.', label= 'f2')
-    ax0.plot(phiVec/2/pi, Xi2[3,:]/1e9, '.', label= 'f3')
+#    ax0.plot(phiVec/2/pi, Xi2[2,:]/1e9, '.', label= 'f2')
+#    ax0.plot(phiVec/2/pi, Xi2[3,:]/1e9, '.', label= 'f3')
     ax0.legend()
     ax0.set_ylabel('GHz')
+    index = np.argmin(np.abs(Xi2[1,:]-Xi2[0,:]-1e9))
+
     
-    fig00, ax00 = plt.subplots(2,2,figsize=(12,6))
-    ax00[0,0].plot(phiVec/2/pi, comp[0, 0,:], '.', label= 'f0/pa')    
-    ax00[0,0].plot(phiVec/2/pi, comp[0, 1,:], '.', label= 'f0/pb')    
-    ax00[0,0].plot(phiVec/2/pi, comp[0, 2,:], '.', label= 'f0/pc')    
-    ax00[0,0].plot(phiVec/2/pi, comp[0, 3,:], '.', label= 'f0/pca')   
-    ax00[0,0].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[0], '.', label= 'norm') 
-
-    ax00[1,0].plot(phiVec/2/pi, comp[1, 0,:], '.', label= 'f1/pa')    
-    ax00[1,0].plot(phiVec/2/pi, comp[1, 1,:], '.', label= 'f1/pb')    
-    ax00[1,0].plot(phiVec/2/pi, comp[1, 2,:], '.', label= 'f1/pc')    
-    ax00[1,0].plot(phiVec/2/pi, comp[1, 3,:], '.', label= 'f1/pca')   
-    ax00[1,0].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[1], '.', label= 'norm') 
-
-    ax00[0,1].plot(phiVec/2/pi, comp[2, 0,:], '.', label= 'f2/pa')    
-    ax00[0,1].plot(phiVec/2/pi, comp[2, 1,:], '.', label= 'f2/pb')    
-    ax00[0,1].plot(phiVec/2/pi, comp[2, 2,:], '.', label= 'f2/pc')    
-    ax00[0,1].plot(phiVec/2/pi, comp[2, 3,:], '.', label= 'f2/pca')   
-    ax00[0,1].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[2], '.', label= 'norm') 
-
-    ax00[1,1].plot(phiVec/2/pi, comp[3, 0,:], '.', label= 'f3/pa')    
-    ax00[1,1].plot(phiVec/2/pi, comp[3, 1,:], '.', label= 'f3/pb')    
-    ax00[1,1].plot(phiVec/2/pi, comp[3, 2,:], '.', label= 'f3/pc')    
-    ax00[1,1].plot(phiVec/2/pi, comp[3, 3,:], '.', label= 'f3/pca')   
-    ax00[1,1].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[3], '.', label= 'norm') 
-
-    ax00[0,0].legend()
-    ax00[0,1].legend()
-    ax00[1,0].legend()    
-    ax00[1,1].legend()
+#    fig00, ax00 = plt.subplots(2,2,figsize=(12,6))
+#    ax00[0,0].plot(phiVec/2/pi, comp[0, 0,:], '.', label= 'f0/pa')    
+#    ax00[0,0].plot(phiVec/2/pi, comp[0, 1,:], '.', label= 'f0/pb')    
+#    ax00[0,0].plot(phiVec/2/pi, comp[0, 2,:], '.', label= 'f0/pc')    
+#    ax00[0,0].plot(phiVec/2/pi, comp[0, 3,:], '.', label= 'f0/pca')   
+#    ax00[0,0].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[0], '.', label= 'norm') 
+#
+#    ax00[1,0].plot(phiVec/2/pi, comp[1, 0,:], '.', label= 'f1/pa')    
+#    ax00[1,0].plot(phiVec/2/pi, comp[1, 1,:], '.', label= 'f1/pb')    
+#    ax00[1,0].plot(phiVec/2/pi, comp[1, 2,:], '.', label= 'f1/pc')    
+#    ax00[1,0].plot(phiVec/2/pi, comp[1, 3,:], '.', label= 'f1/pca')   
+#    ax00[1,0].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[1], '.', label= 'norm') 
+#
+#    ax00[0,1].plot(phiVec/2/pi, comp[2, 0,:], '.', label= 'f2/pa')    
+#    ax00[0,1].plot(phiVec/2/pi, comp[2, 1,:], '.', label= 'f2/pb')    
+#    ax00[0,1].plot(phiVec/2/pi, comp[2, 2,:], '.', label= 'f2/pc')    
+#    ax00[0,1].plot(phiVec/2/pi, comp[2, 3,:], '.', label= 'f2/pca')   
+#    ax00[0,1].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[2], '.', label= 'norm') 
+#
+#    ax00[1,1].plot(phiVec/2/pi, comp[3, 0,:], '.', label= 'f3/pa')    
+#    ax00[1,1].plot(phiVec/2/pi, comp[3, 1,:], '.', label= 'f3/pb')    
+#    ax00[1,1].plot(phiVec/2/pi, comp[3, 2,:], '.', label= 'f3/pc')    
+#    ax00[1,1].plot(phiVec/2/pi, comp[3, 3,:], '.', label= 'f3/pca')   
+#    ax00[1,1].plot(phiVec/2/pi, ((comp**2).sum(0)**0.5)[3], '.', label= 'norm') 
+#
+#    ax00[0,0].legend()
+#    ax00[0,1].legend()
+#    ax00[1,0].legend()    
+#    ax00[1,1].legend()
     
-    print('\nf0 = %.3f GHz\n'%(Xi2[0,:][0]/1e9)+'f1 = %.3f GHz\n'%(Xi2[1,:][0]/1e9)+'f2 = %.3f GHz\n'%(Xi2[2,:][0]/1e9)+'f3 = %.3f GHz\n'%(Xi2[3,:][0]/1e9))
+    print('\nf0 = %.3f GHz\n'%(Xi2[0,:][0]/1e9)+'f1 = %.3f GHz'%(Xi2[1,:][0]/1e9))
+    print('k0 = %.3f MHz\n'%(Xi4[0,:][0]/1e6)+'k1 = %.3f MHz\n'%(Xi4[1,:][0]/1e6)+'k01 = %.3f MHz'%(Xi_a2s2[0]/1e6))
     
-    fig, ax = plt.subplots(4, 4, figsize=(16,8))
+    print('\nphi_ext_opt = %.3f'%(phiVec[index]/2/pi))
+    print('f0 = %.3f GHz\n'%(Xi2[0,index]/1e9)+'f1 = %.3f GHz'%(Xi2[1,index]/1e9))
+    print('k0 = %.3f MHz\n'%(Xi4[0,index]/1e6)+'k1 = %.3f MHz\n'%(Xi4[1,index]/1e6)+'k01 = %.3f MHz'%(Xi_a2s2[index]/1e6))
+    
+    fig, ax = plt.subplots(2, 4, figsize=(16,8))
     ax[0,0].plot(phiVec/2/pi, Xi2[0,:]/1e9, '.', label= 'f0')
     ax[1,0].plot(phiVec/2/pi, Xi2[1,:]/1e9, '.', label= 'f1')
-    ax[2,0].plot(phiVec/2/pi, Xi2[2,:]/1e9, '.', label= 'f2')
-    ax[3,0].plot(phiVec/2/pi, Xi2[3,:]/1e9, '.', label= 'f3')
     ax[0,0].set_title('freq')
 
     ax[0,0].legend()
     ax[1,0].legend()
-    ax[2,0].legend()
-    ax[3,0].legend()
-    
+
     ax[0,1].plot(phiVec/2/pi, Xi3[0,:]/1e6)    
     ax[1,1].plot(phiVec/2/pi, Xi3[1,:]/1e6)
-    ax[2,1].plot(phiVec/2/pi, Xi3[2,:]/1e6)
-    ax[3,1].plot(phiVec/2/pi, Xi3[3,:]/1e6)
     ax[0,1].set_title('c3')
     
     ax[0,2].plot(phiVec/2/pi, Xi4[0,:]/1e6)    
     ax[1,2].plot(phiVec/2/pi, Xi4[1,:]/1e6)
-    ax[2,2].plot(phiVec/2/pi, Xi4[2,:]/1e6)
-    ax[3,2].plot(phiVec/2/pi, Xi4[3,:]/1e6)
     ax[0,2].set_title('c4')
 
     fig2, ax2 = plt.subplots(2,2, figsize=(16,8))
-    ax2[0,0].plot(phiVec/2/pi, Xi_pa2pb/1e6, '.', label= '$a^2b^{+}$')
+    ax2[0,0].plot(phiVec/2/pi, Xi_a2s/1e6, '.', label= '$a^2b^{+}$')
     
-    dXi_pa2pb=np.diff(Xi_pa2pb)/(phiVec[1]-phiVec[0])*pi/10
+    dXi_a2s=np.diff(Xi_a2s)/(phiVec[1]-phiVec[0])*pi/10
     dphiVec = (phiVec[1:]+phiVec[:-1])/2
     
-    ax2[0,0].plot(dphiVec/2/pi, dXi_pa2pb/1e6, '.', label= r'$\frac{\pi}{10}*da^2b^{+}$')
+    ax2[0,0].plot(dphiVec/2/pi, dXi_a2s/1e6, '.', label= r'$\frac{\pi}{10}*da^2b^{+}$')
     ax2[0,0].legend()
     ax2[0,0].set_ylabel('MHz')
     
-    Xi4_1 = (Xi4[1,1:]+Xi4[1,:-1])/2
-    ax2[1,0].plot(dphiVec/2/pi, Xi4_1/1e6, '.', label= '$a^2a^{+2}$')
+    Xi4_0 = (Xi4[0,1:]+Xi4[0,:-1])/2
+    ax2[1,0].plot(dphiVec/2/pi, Xi4_0/1e6, '.', label= '$a^2a^{+2}$')
     ax2[1,0].plot([min(dphiVec/2/pi), max(dphiVec/2/pi)], [0,0])
     ax2[1,0].legend()
     ax2[1,0].set_ylabel('MHz')
 
-    ax2[1,1].plot(phiVec/2/pi, Xi_ac/1e6, '.', label= r'$a^{+}ac^{+}c$')
-    ax2[1,1].plot(phiVec/2/pi, Xi_bc/1e6, '.', label= r'$b^{+}bc^{+}c$')
+    ax2[1,1].plot(phiVec/2/pi, Xi_a2s2/1e6, '.', label= r'$a^{+}ac^{+}c$')
+#    ax2[1,1].plot(phiVec/2/pi, Xi_bc/1e6, '.', label= r'$b^{+}bc^{+}c$')
     ax2[1,1].legend()
+    print('a2b = %.3f MHz\n'%(dXi_a2s[index]/1e6))
     
-    ax2[0,1].plot(dphiVec/2/pi, dXi_pa2pb/Xi4_1, '.', label= '$a^2b^{+}/a^2a^{+2}$')
+    ax2[0,1].plot(dphiVec/2/pi, dXi_a2s/Xi4_0, '.', label= '$da^2b^{+}/a^2a^{+2}$')
 #    ax[3,1].plot(phiVec/2/pi, Xi4[1,:]/1e6, label= '$b^2b^{+2}$')
     ax2[0,1].legend()
 